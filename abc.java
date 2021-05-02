a4d8f4e38abd732e06680a549cd666b7bbb8b7ca
Alankrit Verma <alankrit386@gmail.com>
2021-05-02 11:27:43 +0530

    h

--- abc.java
+++ abc.java
@@ -1,4 +1,3 @@
-
 public class abc
 {
     public static void main(String[] args){
---
+++ nooooo.java
@@ -0,0 +1,6 @@
+class nooooo {
+    public static void main(String[] args) {
+        System.out.println("hellow ");
+    }
+    
+}
