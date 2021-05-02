039dde5fbf4d0c71267e37c06f371e082ad0d055
Alankrit Verma <alankrit386@gmail.com>
2021-05-02 12:01:03 +0530

    not

--- abc.java
+++ abc.java
@@ -1,9 +1,10 @@
 public class abc
 {
     public static void main(String[] args){
-         if(12%7==1) 
-            System.out.println("99");
-        
+         if(12%7==1)
+         {
+             System.out.println("helo");
+         }
             System.out.println("ab");
     
     }
