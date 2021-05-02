203babda36ea159599587e75ce85061a9ce26b36
Alankrit Verma <alankrit386@gmail.com>
2021-05-02 11:17:16 +0530

    fun

---
+++ abc.java
@@ -0,0 +1,12 @@
+
+public class abc
+{
+    public static void main(String[] args){
+         if(12%7==1) 
+            System.out.println("99");
+        
+            System.out.println("ab");
+    
+    }
+   
+}
